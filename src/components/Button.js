4ac23f3f8fdfa233d334e48d205997448e6bcc01
Mandy Schwarze <mndyscode@me.com>
2021-02-24 23:35:50 +0100

    Do npmi and npmi uuid

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,7 +1,7 @@
 import styled from 'styled-components/macro'
 
 export default styled.button`
-  background-color: ${props => (props.isActive ? '#333' : 'lightgray')};
+  background-color: ${props => (props.isActive ? '#fb1' : 'lightgray')};
   ${props => props.isActive && 'color: white;'}
   border: 1px solid black;
   width: 100%;
